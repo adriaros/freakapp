ae3dca726f619f7f454c83abc1c0512f8bfebb07
AdriaRos <adria.ros@elma.care>
2020-07-17 18:20:43 +0200

    master
    - 1.0.0
    - Init project

---
+++ Freakapp/Freakapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Freakapp
+//
+//  Created by Adrià Ros on 17/07/2020.
+//  Copyright © 2020 adria.engineer.ios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
